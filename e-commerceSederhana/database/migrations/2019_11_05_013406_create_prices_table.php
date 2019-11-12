b865a379a0df34f6d004f49602d714397e12f431
fesasabil <kijee212@gmail.com>
2019-11-12 09:53:36 +0700

    update_migration_prices

--- e-commerceSederhana/database/migrations/2019_11_05_013406_create_prices_table.php
+++ e-commerceSederhana/database/migrations/2019_11_05_013406_create_prices_table.php
@@ -17,7 +17,7 @@ class CreatePricesTable extends Migration
             $table->increments('id');
             $table->integer('pricetable_id');
             $table->string('pricetable_type');
-            $table->integer('user_id');
+            $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
